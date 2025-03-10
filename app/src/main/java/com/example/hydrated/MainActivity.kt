e9417d367855e956f7e8c7723ab190f9a1bd3fd7
amr._.dev <nasseramraoui2004@gmail.com>
2025-03-10 06:03:11 +0000

    another one.

--- app/src/main/java/com/example/hydrated/MainActivity.kt
+++ app/src/main/java/com/example/hydrated/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.hydrated.ui.theme.HydratedTheme
+import com.example.hydrated.viewModel.SplashViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<SplashViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+        }
         enableEdgeToEdge()
         setContent {
             HydratedTheme {
